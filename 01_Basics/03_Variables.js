0776ac38e748f83cf4cf30d320ffe1fb7d60a15e
Binod Dey <biniddey06@gmail.com>
2026-03-01 11:15:58 +0530

    feat: Variables in JS: var, let, and const

---
+++ 01_Basics/03_Variables.js
@@ -0,0 +1,10 @@
+// Variables store data
+
+var a = 10; //old way
+let b = 20; //new way, can be reassigned
+const c = 30; //new way, cannot be reassigned
+
+// Difference between var, char, and const
+// 1. Scope: var is function-scoped, while let and const are block-scoped.
+// 2. Reassignment: var and let can be reassigned, while const cannot be reassigned.
+// 3. Redeclare: var can be redeclared, while let and const cannot be redeclared in the same scope.
