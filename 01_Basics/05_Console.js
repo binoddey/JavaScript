8f0b07c8d93019934b88f750ca6341491affa182
Binod Dey <biniddey06@gmail.com>
2026-04-29 00:10:14 +0530

    feat(console.log in js): Exploring console.log()

---
+++ 01_Basics/05_Console.js
@@ -0,0 +1,16 @@
+console.log("Hello World!") // This will print "Hello World!" to the console
+
+// You can also print variables to the console
+let name = "Binod Dey";
+console.log(name); // This will print the value of the variable 'name' to the console
+
+// You can also print multiple values to the console
+let age = 20;
+console.log("Name:", name, "Age:", age); // This will print "Name: Binod Dey Age: 20" to the console
+console.log("Name:", name, "\nAge:", age); // This will print "Name: Binod Dey" and "Age: 20" on separate lines in the console
+
+// You can also use template literals to print variables to the console
+console.log(`Name: ${name}\nAge: ${age}`); // This will print "Name: Binod Dey" and "Age: 20" on separate lines in the console
+
+// You can also use console.error() to print error messages to the console
+console.error("This is an error message!"); // This will print "This is an error message!" in red color in the console
