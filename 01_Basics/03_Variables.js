6905b8691a1006aa2f903af7de5ce2ab1881e108
Binod Dey <biniddey06@gmail.com>
2026-04-29 00:06:48 +0530

    feat(Variables in JS): Learn more about variables in javascript

--- 01_Basics/03_Variables.js
+++ 01_Basics/03_Variables.js
@@ -8,3 +8,12 @@ const c = 30; //new way, cannot be reassigned
 // 1. Scope: var is function-scoped, while let and const are block-scoped.
 // 2. Reassignment: var and let can be reassigned, while const cannot be reassigned.
 // 3. Redeclare: var can be redeclared, while let and const cannot be redeclared in the same scope.
+
+var my_name = "John"
+var myname = "John"
+var $1223 = 12344
+// var 123myage = 20 // This will throw an error because variable names cannot start with a number
+// var my-age = 20 // This will throw an error because variable names cannot contain hyphens
+var my_age = 20 // This is a valid variable name
+var $city = "Jaipur"
+// var my@email = 'bd@gmail.com' // This will throw an error because variable names cannot contain the @ symbol
