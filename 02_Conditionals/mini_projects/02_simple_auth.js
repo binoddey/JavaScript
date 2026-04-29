db990881c3049072b50be178cc73f26dcf844b64
Binod Dey <biniddey06@gmail.com>
2026-04-29 21:23:53 +0530

    feat(Project 2): A simple Authentication system in JS

---
+++ 02_Conditionals/mini_projects/02_simple_auth.js
@@ -0,0 +1,14 @@
+let username = "admin";
+let password = "12345";
+
+if (username === "admin") {
+
+    if (password === "12345") {
+        console.log("Login Successful");
+    } else {
+        console.log("Incorrect Password");
+    }
+
+} else {
+    console.log("Invalid Username");
+}
