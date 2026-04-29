c9ae5c7554b16cdd018058747819c6d0046bd54b
Binod Dey <biniddey06@gmail.com>
2026-04-29 21:18:47 +0530

    feat(consitional statements): Use of if statements in JS

---
+++ 02_Conditionals/01_if_statement.js
@@ -0,0 +1,35 @@
+/* What is an IF statement?
+The if statement is used to execute a block of code only when a condition is true.
+*/
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+}
+
+//===============================
+
+let number = 10;
+
+if (number > 0) {
+    console.log("The number is positive.");
+}
+
+//===============================
+
+let password = "javascript123";
+
+if (password.length >= 8) {
+    console.log("Strong password.");
+}
+
+//===============================
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("Welcome back!");
+}
+
+//===============================
