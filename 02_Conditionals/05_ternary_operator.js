6aa0f6d787cdfa34c5ba08ccedb11f16eaef2be7
Binod Dey <biniddey06@gmail.com>
2026-04-29 21:22:13 +0530

    feat(Conditional statements): Use of ternary operators in JS

---
+++ 02_Conditionals/05_ternary_operator.js
@@ -0,0 +1,22 @@
+// The ternary operator is a shorthand version of if...else.
+// It is mainly used for short conditions.
+
+// ==========================================
+
+/* Ternary Operator Syntax:
+
+condition ? trueValue : falseValue;
+
+*/
+
+// ==========================================
+
+// EXAMPLE 1: Even or Odd
+
+let number = 7;
+
+let result = (number % 2 === 0)
+    ? "Even"
+    : "Odd";
+
+console.log(result);
