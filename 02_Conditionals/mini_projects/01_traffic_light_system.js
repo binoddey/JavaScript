1248c5f829b64416741b246d70be9b6dbc34816c
Binod Dey <biniddey06@gmail.com>
2026-04-29 21:23:08 +0530

    feat(Project 1): A Traffic Light System

---
+++ 02_Conditionals/mini_projects/01_traffic_light_system.js
@@ -0,0 +1,11 @@
+let signal = "green";
+
+if (signal === "red") {
+    console.log("STOP");
+} else if (signal === "yellow") {
+    console.log("WAIT");
+} else if (signal === "green") {
+    console.log("GO");
+} else {
+    console.log("Invalid signal");
+}
